f928259bf544c1eb5236507b72c3efc60666c575
KieuLong-UET <kieunhatlong@gmail.com>
2022-12-08 22:27:52 +0700

    catch exception about promis function

--- BE/controllers/productController.js
+++ BE/controllers/productController.js
@@ -1,99 +1,105 @@
 const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors")
 
 //Create Product  -- Admin
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncErrors(
+    async (req, res, next) => {
 
-    const product = await Product.create(req.body);
-
-    res.status(201).json({
-        success: true,
-        product
-    })
-}
-
-//Get all product --admin
-
-exports.getAllProducts = async (req, res) => {
+        const product = await Product.create(req.body);
     
-    const products = await Product.find();
-    res.status(200).json({
-        success: true,
-        products
-    });
-
-}
-
-
-//Get product detail
-
-exports.getProductDetails = async (req, res, next) => {
-
-    try {
-        const product = await Product.findById(req.params.id);
-    
-    if(!product) {
-        return next(new ErrorHander("Product not found", 404));
-    }
-
-    if(product) {
-        return res.status(200).json({
+        res.status(201).json({
             success: true,
             product
         })
     }
-    } catch (error) {
-        return next(new ErrorHander());
-    }
+)
 
-}
+//Get all product --admin
+
+exports.getAllProducts = catchAsyncErrors(
+    async (req, res) => {
+    
+        const products = await Product.find();
+        res.status(200).json({
+            success: true,
+            products
+        });
+    
+    }
+)
+
+
+//Get product detail
+
+exports.getProductDetails = catchAsyncErrors(
+    async (req, res, next) => {
+
+        const product = await Product.findById(req.params.id);
+        
+        if(!product) {
+            return next(new ErrorHander("Product not found", 404));
+        }
+    
+        if(product) {
+            return res.status(200).json({
+                success: true,
+                product
+            })
+        }
+    }
+)
 
 
 //Update product
 
-exports.updateProduct = async (req, res) => {
+exports.updateProduct = catchAsyncErrors(
+    async (req, res) => {
     
-    let product = Product.findById(req.params.id);
-
-    if(!product) {
-        return res.status(500).json({
-            success: false,
-            message: "Product not found"
+        let product = Product.findById(req.params.id);
+    
+        if(!product) {
+            return res.status(500).json({
+                success: false,
+                message: "Product not found"
+            })
+        }
+    
+        product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+        });
+    
+        res.status(200).json({
+            success: true,
+            product
         })
+    
     }
-
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false
-    });
-
-    res.status(200).json({
-        success: true,
-        product
-    })
-
-}
+)
 
 
 //Delete product
 
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(
+    async (req, res, next) => {
 
-    const product = await Product.findById(req.params.id);
-
-    if(!product) {
-        return res.status(500).json({
-            success: false,
-            message: "Product not found"
+        const product = await Product.findById(req.params.id);
+    
+        if(!product) {
+            return res.status(500).json({
+                success: false,
+                message: "Product not found"
+            })
+        }
+    
+        await product.remove();
+    
+        res.status(200).json({
+            success: true,
+            message: "Xoa thanh cong san pham"
         })
+    
     }
-
-    await product.remove();
-
-    res.status(200).json({
-        success: true,
-        message: "Xoa thanh cong san pham"
-    })
-
-}
+)
