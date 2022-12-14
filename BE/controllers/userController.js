f1cef119708553854ccad40d133a77cafaf8a0a4
long.kieunhat <long.kieunhat@vti.com.vn>
2022-12-14 13:42:56 +0700

    get user detail

--- BE/controllers/userController.js
+++ BE/controllers/userController.js
@@ -146,3 +146,15 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 
     sendToken(user, 200, res);
 })
+
+//user details
+
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+    
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
