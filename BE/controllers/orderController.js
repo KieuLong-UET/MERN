74ef6c6f036f1c533c2f2cfc240d8ca744b1cdb8
long.kieunhat <long.kieunhat@vti.com.vn>
2022-12-26 16:36:30 +0700

    crud order - admin

--- BE/controllers/orderController.js
+++ BE/controllers/orderController.js
@@ -60,3 +60,76 @@ exports.myOrders = catchAsyncErrors(async (req, res, next) => {
     orders,
   });
 });
+
+//Get all order -- admin
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+
+  let orderslength = orders.length;
+
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orderslength,
+    orders,
+  });
+});
+
+//Update order status -- admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order khong ton tai", 404));
+  }
+
+  if(order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("Order da duoc giao", 400));
+  }
+
+  order.orderItems.forEach(async order => {
+    await updateStock(order.product, order.quantity);
+  })
+
+  order.orderStatus = req.body.status;
+
+  if(req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({validateBeforeSave: false});
+
+  res.status(200).json({
+    success: true,
+    order
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock -= quantity;
+
+  await product.save({validateBeforeSave: false});
+}
+
+//Delete order -- admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order khong ton tai", 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true
+  });
+});
--- BE/controllers/productController.js
+++ BE/controllers/productController.js
@@ -72,7 +72,7 @@ exports.updateProduct = catchAsyncErrors(
         let product = Product.findById(req.params.id);
     
         if(!product) {
-            return res.status(500).json({
+            return res.status(404).json({
                 success: false,
                 message: "Product not found"
             })
