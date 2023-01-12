0e5250136e518feec1f7a976fa1ebb6c0f0458a3
long.kieunhat <long.kieunhat@vti.com.vn>
2023-01-12 14:32:05 +0700

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/layout/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <Router>
+      <Header />
+    </Router>
   );
 }
 
