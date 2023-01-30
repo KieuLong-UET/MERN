9ae45ab8209c56d2ccc6e099ac66abf113017601
long.kieunhat <long.kieunhat@vti.com.vn>
2023-01-30 17:08:37 +0700

    add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
-import Header from "./components/layout/Header.js";
+import Header from "./components/layout/Header/Header.js";
+import Footer from "./components/layout/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
   return (
     <Router>
       <Header />
+      <Footer />
     </Router>
   );
 }
--- frontend/src/components/layout/Header/Header.js
+++ frontend/src/components/layout/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ReactNavbar } from "overlay-navbar";
-import logo from "../../images/logo512.png";
+import logo from "../../../images/logo512.png";
 
 const options = {
   burgerColorHover: "#eb4034",
