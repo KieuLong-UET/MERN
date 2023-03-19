760521c1ed2f4d3b7e41bddd4f6fc8471c470764
KieuLong-UET <kieunhatlong@gmail.com>
2023-03-20 02:15:20 +0700

    header

---
+++ eco-frontend/src/components/header/IconGroup.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const IconGroup = () => {
+  return (
+    <div className={`header-right-wrap`}>
+      <div className="same-style header-search d-none d-lg-block">
+        <button className="search-active">
+          <i className="pe-7s-search" />
+        </button>
+        <div className="search-content">
+          <form action="#">
+            <input type="text" placeholder="Search" />
+            <button className="button-search">
+              <i className="pe-7s-search" />
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="same-style account-setting d-none d-lg-block">
+        <button className="account-setting-active">
+          <i className="pe-7s-user-female" />
+        </button>
+        <div className="account-dropdown">
+          <ul>
+            <li>
+              <Link to={process.env.PUBLIC_URL + "/login-register"}>Login</Link>
+            </li>
+            <li>
+              <Link to={process.env.PUBLIC_URL + "/login-register"}>
+                Register
+              </Link>
+            </li>
+            <li>
+              <Link to={process.env.PUBLIC_URL + "/my-account"}>
+                my account
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="same-style header-wishlist">
+        <Link to={process.env.PUBLIC_URL + "/wishlist"}>
+          <i className="pe-7s-like" />
+          <span className="count-style">
+            {wishlistData && wishlistData.length ? wishlistData.length : 0}
+          </span>
+        </Link>
+      </div>
+      <div className="same-style cart-wrap d-none d-lg-block">
+        <button className="icon-cart">
+          <i className="pe-7s-shopbag" />
+        </button>
+        {/* menu cart */}
+      </div>
+      <div className="same-style cart-wrap d-block d-lg-none">
+        <Link className="icon-cart" to={process.env.PUBLIC_URL + "/cart"}>
+          <i className="pe-7s-shopbag" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default IconGroup;
--- eco-frontend/src/wrapers/header/HeaderMain.js
+++ eco-frontend/src/wrapers/header/HeaderMain.js
@@ -1,9 +1,54 @@
-const HeaderMain = () => {
-    return ( 
-        <div>
-            <h1>LongK HeaderMain</h1>
+import { Link } from "react-router-dom";
+import IconGroup from "../../components/header/IconGroup";
+
+const imageUrl = "/assets/img/logo/logo.png";
+
+const HeaderMain = ({}) => {
+  return (
+    <header className="header-area clearfix header-hm9 transparent-bar">
+      <div className="container">
+        <div className="header-top-area d-none d-lg-block">
+          <div className="row">
+            <div className="col-lg-2 d-none d-lg-block text-center">
+              {/* header logo */}
+              <div>
+                <Link to={process.env.PUBLIC_URL + "/"}>
+                  <img alt="" src={process.env.PUBLIC_URL + imageUrl} />
+                </Link>
+              </div>
+            </div>
+            <div className="col-lg-5 col-md-4 col-12">
+              {/* Icon group */}
+              <IconGroup />
+            </div>
+          </div>
         </div>
-     );
-}
+      </div>
+      <div
+        className={`header-bottom sticky-bar header-res-padding header-padding-2 ${
+          scroll > headerTop ? "stick" : ""
+        }`}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-6 d-block d-lg-none">
+              {/* header logo */}
+              <div className="logo-hm-9">
+                <Link to={process.env.PUBLIC_URL + "/"}>
+                  <img alt="" src={process.env.PUBLIC_URL + imageUrl} />
+                </Link>
+              </div>
+            </div>
+            <div className="col-6 d-block d-lg-none">
+              {/* Icon group */}
+              <IconGroup />
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default HeaderMain;
