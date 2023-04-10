63f668ef98feef3ec11b831e835ac67fb14b808a
long.kieunhat <long.kieunhat@vti.com.vn>
2023-04-10 17:31:48 +0700

    footer

--- eco-frontend/src/layouts/LayoutMain.js
+++ eco-frontend/src/layouts/LayoutMain.js
@@ -1,12 +1,14 @@
 import PropTypes from "prop-types";
 import React, { Fragment } from "react";
 import HeaderMain from "../wrapers/header/HeaderMain";
+import Footer from "../wrapers/footer/Footer";
 
 const LayoutMain = ({ children }) => {
   return (
     <Fragment>
       <HeaderMain />
       {children}
+      <Footer />
     </Fragment>
   );
 };
