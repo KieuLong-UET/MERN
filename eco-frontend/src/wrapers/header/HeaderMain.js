23d6d7929614f21db0a0006f08f9ab2571f8d1bd
long.kieunhat <long.kieunhat@vti.com.vn>
2023-04-10 16:33:22 +0700

    header

--- eco-frontend/src/wrapers/header/HeaderMain.js
+++ eco-frontend/src/wrapers/header/HeaderMain.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import logo from "./logo.png"
+import logo from "./logo.png";
 import IconGroup from "../../components/header/IconGroup";
 
 const HeaderMain = () => {
@@ -10,15 +10,20 @@ const HeaderMain = () => {
         <div className="header-top-area d-none d-lg-block">
           <div className="row">
             <div className="col-lg-5 col-md-8 col-12">
-              {/* Call us */}
-              <div className="same-language-currency">
-                <p>Liên hệ: 038591xxx</p>
+              {/* Số điện thoại */}
+              <div className="language-currency-wrap">
+                <div className="same-language-currency">
+                  <span>Việt Nam</span>
+                </div>
+                <div className="same-language-currency">
+                  <span>Liên hệ 03859xxxxx</span>
+                </div>
               </div>
             </div>
             <div className="col-lg-2 d-none d-lg-block text-center">
               {/* header logo */}
               <div>
-                <img src={logo} alt="Logo"/>
+                <img src={logo} alt="Logo" />
               </div>
             </div>
             <div className="col-lg-5 col-md-4 col-12">
