45374699e11a24641320f6eb7ab57204665b291d
long.kieunhat <long.kieunhat@vti.com.vn>
2023-04-13 10:56:40 +0700

    done homepage but not data product

--- eco-frontend/src/pages/home/HomeMain.js
+++ eco-frontend/src/pages/home/HomeMain.js
@@ -3,7 +3,7 @@ import MetaTags from "react-meta-tags";
 import LayoutMain from "../../layouts/LayoutMain";
 import Slider from "../../wrapers/slider/Slider";
 import Category from "../../wrapers/category/Category";
-import TabProduct from "../../wrapers/product/TabProduct";
+// import TabProduct from "../../wrapers/product/TabProduct";
 import LogoBrandSlider from "../../wrapers/logobrand/LogoBrandSlider";
 
 const HomeMain = () => {
@@ -19,7 +19,7 @@ const HomeMain = () => {
         {/* category */}
         <Category spaceBottomClass="pb-95" />
 
-        {/* tab product */}
+        {/* tab product ---> Đoạn này phải call API để get data, khá mệt ----> để sau */}
         {/* <TabProduct spaceBottomClass="pb-60" category="electronics" /> */}
 
         {/* logo brand */}
